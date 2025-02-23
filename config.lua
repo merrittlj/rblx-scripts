3708274a0a411648a5b27076bd5bcd7e744a89f1
Lucas Merritt <Lucas Merritt>
2025-02-22 22:27:31 -0800

    invisible fling

--- config.lua
+++ config.lua
@@ -1,8 +1,9 @@
 local MenuName = 'RBLXHAX'
 local Modules = {
-	['UtilAimbotEnable'] = false,
-	['GamesBPBulletTP'] = false,
-	['GamesWBType'] = false,
+    ['UtilAimbotEnable'] = false,
+    ['UtilFlingEnable'] = false,
+    ['GamesMMCooldown'] = false,
+    ['GamesWBType'] = false,
 }
 
 -- Linoria Lib
@@ -80,34 +81,74 @@ UtilAimbotGroupBox:AddInput('UtilAimbotWait', {
 })
 
 
+local UtilFlingGroupBox = Tabs.Util:AddLeftGroupbox('Invisible Fling')
 
-local GamesBPGroupbox = Tabs.Games:AddLeftGroupbox('Big Paintball')
-
-GamesBPGroupbox:AddToggle('GamesBPBulletTP', {
-    Text = 'Enable bullet TP',
+UtilFlingGroupBox:AddToggle('UtilFlingEnable', {
+    Text = 'Enable module',
     Default = false,
-    Tooltip = 'Teleports bullets inside of enemies, good kill all',
-    Callback = function(v) Modules["GamesBPBulletTP"] = v end
+    Tooltip = 'Enable module',
+    Callback = function(v) Modules["UtilFlingEnable"] = v end
 })
 
-GamesBPGroupbox:AddLabel('Bullet TP bind'):AddKeyPicker('GamesBPBulletActivate', {
-    Default = 'LeftShift',
+UtilFlingGroupBox:AddLabel('Start/Stop bind'):AddKeyPicker('UtilFlingSS', {
+    Default = 'F',
     SyncToggleState = false,
 
-    Mode = 'Hold',
+    Mode = 'Toggle',
 
-    Text = 'Activate bullet TP',
+    Text = 'Start/Stop Fling',
     NoUI = false,
 })
 
-GamesBPGroupbox:AddLabel('Shooting bind'):AddKeyPicker('GamesBPShooting', {
-    Default = 'MB1',
+UtilFlingGroupBox:AddLabel('Move forward'):AddKeyPicker('UtilFlingF', {
+    Default = 'W',
     SyncToggleState = false,
 
     Mode = 'Hold',
 
-    Text = 'If shooting, DO NOT CHANGE',
+    Text = 'Move forward bind, DO NOT CHANGE',
     NoUI = true,
+})
+
+UtilFlingGroupBox:AddLabel('Move backward'):AddKeyPicker('UtilFlingB', {
+    Default = 'S',
+    SyncToggleState = false,
+
+    Mode = 'Hold',
+
+    Text = 'Move backward bind, DO NOT CHANGE',
+    NoUI = true,
+})
+
+UtilFlingGroupBox:AddLabel('Move left'):AddKeyPicker('UtilFlingL', {
+    Default = 'A',
+    SyncToggleState = false,
+
+    Mode = 'Hold',
+
+    Text = 'Move left bind, DO NOT CHANGE',
+    NoUI = true,
+})
+
+UtilFlingGroupBox:AddLabel('Move right'):AddKeyPicker('UtilFlingR', {
+    Default = 'D',
+    SyncToggleState = false,
+
+    Mode = 'Hold',
+
+    Text = 'Move right bind, DO NOT CHANGE',
+    NoUI = true,
+})
+
+
+
+local GamesMMGroupbox = Tabs.Games:AddLeftGroupbox('Mortem Metallum')
+
+GamesMMGroupbox:AddToggle('GamesMMCooldown', {
+    Text = 'Enable no cooldown',
+    Default = false,
+    Tooltip = 'No cooldown on crossbow shooting',
+    Callback = function(v) Modules["GamesMMCooldown"] = v end
 })
 
 
@@ -190,30 +231,30 @@ end)
 
 
 function ApplyTheme()
-	local data = '{"FontColor":"ffffff","MainColor":"0a0a0a","AccentColor":"ee0000","BackgroundColor":"000000","OutlineColor":"323232"}'
-	local scheme = game:GetService('HttpService'):JSONDecode(data)
-	for idx, col in next, scheme do
-		Library[idx] = Color3.fromHex(col)
-		
-		if Options[idx] then
-			Options[idx]:SetValueRGB(Color3.fromHex(col))
-		end
-	end
+    local data = '{"FontColor":"ffffff","MainColor":"0a0a0a","AccentColor":"ee0000","BackgroundColor":"000000","OutlineColor":"323232"}'
+    local scheme = game:GetService('HttpService'):JSONDecode(data)
+    for idx, col in next, scheme do
+        Library[idx] = Color3.fromHex(col)
+        
+        if Options[idx] then
+            Options[idx]:SetValueRGB(Color3.fromHex(col))
+        end
+    end
 
-	ThemeUpdate()
+    ThemeUpdate()
 end
 
 function ThemeUpdate()
-	-- This allows us to force apply themes without loading the themes tab :)
-	local options = { "FontColor", "MainColor", "AccentColor", "BackgroundColor", "OutlineColor" }
-	for i, field in next, options do
-		if Options and Options[field] then
-			Library[field] = Options[field].Value
-		end
-	end
+    -- This allows us to force apply themes without loading the themes tab :)
+    local options = { "FontColor", "MainColor", "AccentColor", "BackgroundColor", "OutlineColor" }
+    for i, field in next, options do
+        if Options and Options[field] then
+            Library[field] = Options[field].Value
+        end
+    end
 
-	Library.AccentColorDark = Library:GetDarkerColor(Library.AccentColor);
-	Library:UpdateColorsUsingRegistry()
+    Library.AccentColorDark = Library:GetDarkerColor(Library.AccentColor);
+    Library:UpdateColorsUsingRegistry()
 end
 
 ApplyTheme()
@@ -225,146 +266,246 @@ ApplyTheme()
 
 local plr = game:GetService("Players").LocalPlayer
 local char = plr.Character
+local root = char:FindFirstChild("HumanoidRootPart")
+local hum = char:FindFirstChild("Humanoid")
 local mouse = plr:GetMouse()
-local camera = game.Workspace.CurrentCamera
+local camera = workspace.CurrentCamera
+
+function getMouseTarget(valf)
+        local ray = camera:ScreenPointToRay(mouse.X, mouse.Y)
+        local raycastResult = game.Workspace:Raycast(ray.Origin, ray.Direction * 50000)
+        local mousePos = raycastResult and raycastResult.Position or ray.Origin
+        local closestTarget = nil
+        local closestDistance = math.huge
+
+        for _, character in pairs(aimbotCharFolder:GetChildren()) do
+            if character:IsA("Model") and character ~= plr.Character then
+                if not valf(character) then continue end
+                -- THIS MUST BE HRP
+                local primaryPart = character.PrimaryPart or character:FindFirstChild("HumanoidRootPart")
+                if primaryPart then
+                    local distance = (primaryPart.Position - mousePos).Magnitude
+                    if distance < closestDistance then
+                        closestDistance = distance
+                        closestTarget = character
+                    end
+                end
+            end
+        end
+
+        return closestTarget
+    end
 
 -- Aimbot
 task.spawn(function()
-	local aimbotCharFolder = game:GetService("Workspace")
+    local aimbotCharFolder = game:GetService("Workspace")
 
-	local function getTarget()
-	    local ray = camera:ScreenPointToRay(mouse.X, mouse.Y)
-	    local raycastResult = game.Workspace:Raycast(ray.Origin, ray.Direction * 50000)
-	    local mousePos = raycastResult and raycastResult.Position or ray.Origin
-	    local closestTarget = nil
-	    local closestDistance = math.huge
+    local function valid(who)
+        local human = who:FindFirstChild("Humanoid")
+        print("checking: " .. who.Name)
+        if human then
+            print(human.Health)
+        end
+        print(aimbotCharFolder:FindFirstChild(who.Name))
+        return who and human and human.Health > 0 and aimbotCharFolder:FindFirstChild(who.Name)
+    end
 
-	    for _, character in pairs(aimbotCharFolder:GetChildren()) do
-	        if character:IsA("Model") and character ~= plr.Character then
-	            if not valid(character) then continue end
-	            -- THIS MUST BE HRP
-	            local primaryPart = character.PrimaryPart or character:FindFirstChild("HumanoidRootPart")
-	            if primaryPart then
-	                local distance = (primaryPart.Position - mousePos).Magnitude
-	                if distance < closestDistance then
-	                    closestDistance = distance
-	                    closestTarget = character
-	                end
-	            end
-	        end
-	    end
+    local function look(who)
+        print("looking at " .. who.Name)
 
-	    return closestTarget
-	end
+        local them = who:FindFirstChild("Head")
+        if not them then
+            print("failed head check")
+            return 
+        end
 
-	local function valid(who)
-	    local hum = who:FindFirstChild("Humanoid")
-	    print("checking: " .. who.Name)
-	    if hum then
-	        print(hum.Health)
-	    end
-	    print(aimbotCharFolder:FindFirstChild(who.Name))
-	    return who and hum and hum.Health > 0 and aimbotCharFolder:FindFirstChild(who.Name)
-	end
-
-	local function look(who)
-	    print("looking at " .. who.Name)
-
-	    local them = who:FindFirstChild("Head")
-	    if not them then
-	        print("failed head check")
-	        return 
-	    end
-
-	    camera.CFrame = CFrame.lookAt(camera.CFrame.Position, them.Position)
-	end
+        camera.CFrame = CFrame.lookAt(camera.CFrame.Position, them.Position)
+    end
     while true do
-    	task.wait()
+        task.wait()
         if Options.UtilAimbotLock:GetState() and Modules["UtilAimbotEnable"] then
-        	print("aimbot active")
-            local target = getTarget()
-	        if not target then return end
-	        print("found target " .. target.Name)
-	        while Options.UtilAimbotLock:GetState() and valid(target) and not Library.Unloaded do
-	            look(target)
-	            task.wait(tonumber(Options.UtilAimbotWait.Value) / 1000.0)
-	        end
-	        repeat task.wait() until not Options.UtilAimbotLock:GetState()
+            print("aimbot active")
+            local target = getMouseTarget(valid)
+            if not target then return end
+            print("found target " .. target.Name)
+            while Options.UtilAimbotLock:GetState() and valid(target) and not Library.Unloaded do
+                look(target)
+                task.wait(tonumber(Options.UtilAimbotWait.Value) / 1000.0)
+            end
+            repeat task.wait() until not Options.UtilAimbotLock:GetState()
         end
 
         if Library.Unloaded then break end
     end
 end)
 
--- Big Paintball Bullet TP
+
+-- Invisible Fling
+local ctrl = {f = 0, b = 0, l = 0, r = 0}
+local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+Options.UtilFlingSS:OnClick(function()
+    local state = Options.UtilFlingSS:GetState()
+    if not Modules["UtilFlingEnable"] or not state then return end
+
+    spawn(
+        function()
+            local message = Instance.new("Message", workspace)
+            message.Text = "Invisible Fling by irobloxianlover (wait 11 seconds to load)"
+            wait(11)
+            message:Destroy()
+        end
+    )
+
+    local prevFrame = root.CFrame
+
+    local prt = Instance.new("Model", workspace)
+
+    local ch = char
+    local z1 = Instance.new("Part", prt)
+    z1.Name = "Torso"
+    z1.CanCollide = false
+    z1.Anchored = true
+
+    local z2 = Instance.new("Part", prt)
+    z2.Name = "Head"
+    z2.Anchored = true
+    z2.CanCollide = false
+
+    local z3 = Instance.new("Humanoid", prt)
+    z3.Name = "Humanoid"
+    z1.Position = Vector3.new(0, 9999, 0)
+    z2.Position = Vector3.new(0, 9991, 0)
+    char = prt
+    wait(5)
+    char = ch
+    wait(6)
+
+    local sel = Instance.new("SelectionBox", root)
+    sel.Adornee = root
+    sel.Color3 = Color3.fromRGB(255,0,0)
+
+    char:FindFirstChild("Humanoid").RequiresNeck = false;
+    local humanoid = Instance.new("Humanoid", plr)
+    humanoid.RequiresNeck = false;
+
+    for _, v in pairs(char:GetChildren()) do
+        if v ~= root and v.Name ~= "Humanoid" then
+            v:Destroy()
+        end
+    end
+
+    camera.CameraSubject = root
+
+    game:GetService("RunService").Stepped:connect(
+        function()
+            root.CanCollide = false
+        end
+    )
+    game:GetService("RunService").RenderStepped:connect(
+        function()
+            root.CanCollide = false
+        end
+    )
+    
+    power = 999999 * 10 -- change this to make it more or less powerful
+
+    ---
+    wait(.1)
+    local bambam = Instance.new("BodyThrust", root)
+    bambam.Force = Vector3.new(power, 0, power)
+    bambam.Location = root.Position
+
+    local flying = true
+    local deb = true
+    local maxspeed = 120
+    local speed = 15
+
+    local bg = Instance.new("BodyGyro", root)
+    bg.P = 9e4
+    bg.maxTorque = Vector3.new(0, 0, 0)
+    bg.cframe = root.CFrame
+    local bv = Instance.new("BodyVelocity", root)
+    bv.velocity = Vector3.new(0, 0, 0)
+    bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+    repeat
+        task.wait()
+
+        if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+            speed = speed + .2
+            if speed > maxspeed then
+                speed = maxspeed
+            end
+        elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+            speed = speed - 1
+            if speed < 0 then
+                speed = 0
+            end
+        end
+        if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+            bv.velocity =
+                ((camera.CoordinateFrame.lookVector * (ctrl.f + ctrl.b)) +
+                ((camera.CoordinateFrame *
+                    CFrame.new(ctrl.l + ctrl.r, (ctrl.f + ctrl.b) * .2, 0).p) -
+                    camera.CoordinateFrame.p)) *
+                speed
+            lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+        elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+            bv.velocity =
+                ((camera.CoordinateFrame.lookVector * (lastctrl.f + lastctrl.b)) +
+                ((camera.CoordinateFrame *
+                    CFrame.new(lastctrl.l + lastctrl.r, (lastctrl.f + lastctrl.b) * .2, 0).p) -
+                    camera.CoordinateFrame.p)) *
+                speed
+        else
+            bv.velocity = Vector3.new(0, 0.1, 0)
+        end
+    until not flying
+    ctrl = {f = 0, b = 0, l = 0, r = 0}
+    lastctrl = {f = 0, b = 0, l = 0, r = 0}
+    speed = 0
+    bg:Destroy()
+    bv:Destroy()
+
+    repeat task.wait() until not Options.UtilFlingSS:GetState()
+
+    -- respawn
+    root.CFrame = prevFrame
+    sel:Destroy()
+end)
+
 task.spawn(function()
-	local bulletsFolder = game:GetService("Workspace")["__DEBRIS"].Projectiles
-	local gunsFolder = game:GetService("Workspace")["__DEBRIS"].Guns
-	local roundType = game:GetService("Workspace")["__VARIABLES"].RoundType.Value:lower()
+    while true do
+        task.wait()
+        if Library.Unloaded then break end
+        if not Modules["UtilFlingEnable"] then continue end
 
-	local function getFirstTarget()
-		plrs = game:GetService("Players")
-		for _, gun in pairs(gunsFolder:GetChildren()) do
-			local target = plrs:FindFirstChild(gun.Name)
-			if roundType:match("ffa") and target.UserId ~= plr.UserId then return target end
-			if roundType:match("tdm") and target.Team ~= plr.Team then return target end
-		end
-	end
-	local function getClosestPart(table)
-	    local root = char:FindFirstChild("HumanoidRootPart")
+        ctrl = {f=0,b=0,l=0,r=0}
+        if Options.UtilFlingF:GetState() then ctrl.f = 1 end
+        if Options.UtilFlingB:GetState() then ctrl.b = -1 end
+        if Options.UtilFlingL:GetState() then ctrl.l = -1 end
+        if Options.UtilFlingR:GetState() then ctrl.r = 1 end
+    end
+end)
 
-	    if not root then
-	        warn("No HumanoidRootPart found!")
-	        return nil
-	    end
-
-	    local closestPart = nil
-	    local shortestDistance = math.huge
-
-	    for _, part in ipairs(table) do
-	        if part:IsA("BasePart") then
-	            local distance = (part.Position - root.Position).Magnitude
-	            if distance < shortestDistance then
-	                shortestDistance = distance
-	                closestPart = part
-	            end
-	        end
-    	end
-
-    	return closestPart
-	end
-
+-- Mortom Metallum No Cooldown
+task.spawn(function()
     while true do
         task.wait()
         
-        if Options.GamesBPBulletActivate:GetState() and Options.GamesBPShooting:GetState() and Modules["GamesBPBulletTP"] then
-            print('check and tp bullets')
-            local target = getFirstTarget()
-            print(target.Name)
-            repeat 
-            	local validBullets = {}
-            	for _, part in pairs(bulletsFolder:GetChildren()) do
-	             	if part.Material ~= Enum.Material.ForceField then continue end
-	             	if roundType:match("tdm") then
-	             		if plr.Team == "Blue" and part.Color == Color3.fromRGB(255, 0, 0) then continue end
-	             		if plr.Team == "Red" and part.Color == Color3.fromRGB(0, 0, 255) then continue end
-	             	end
-
-	             	part.Anchored = false
-	             	part.CFrame = CFrame.new(Vector3.new(0,0,0))
-	             	table.insert(validBullets, part)
-            	end
-
-            	local closestBullet = getClosestPart(validBullets)
-            	print("bullet pos: " .. tostring(closestBullet.Position))
-            	print("player pos: " .. tostring(char:FindFirstChild("HumanoidRootPart").Position))
-
-            	-- TP Bullet
-            	--closestBullet.Position = target.Character:FindFirstChild("HumanoidRootPart").Position
-            	closestBullet.Position = Vector3.new(0,0,0)
-
-            	break
-            until not gunsFolder:FindFirstChild(target.Name)
+        if Modules["GamesMMCooldown"] then
+            local plrchar = workspace["PlayersCharacters"]:FindFirstChild(plr.Name)
+            local tool = nil
+            if plrchar then tool = plrchar:FindFirstChildOfClass("Tool") end
+            if tool then 
+                --tool.Cooldowntime.Value = 0
+                tool:WaitForChild("Reloaded").Value = true
+                local m = tool:WaitForChild("mouse")
+                local dir = Vector3.new(0,0,0)
+                local target = workspace["PlayersCharacters"]:FindFirstChild("iamsocoolunlikeuhaha")
+                local pos = target.HumanoidRootPart.CFrame
+                m:FireServer(dir, pos, 'S')
+            end
+                --m:FireServer(nil, nil, 'R')
         end
 
         if Library.Unloaded then break end
@@ -375,8 +516,8 @@ alreadyUsed = {}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MarsQQ/Unpatchabomb/master/unpatchabomb_english_words", true))()
 -- Word Bomb
 function WordBombActivate()
-	if not Modules["GamesWBType"] then return end
-	local function tableItemExists(array, val)
+    if not Modules["GamesWBType"] then return end
+    local function tableItemExists(array, val)
         for i,v in pairs (array) do 
             if v == val then 
                 return true
@@ -396,7 +537,7 @@ function WordBombActivate()
         return found
     end
     local textbox = plr.PlayerGui.GameUI.Container.GameSpace.DefaultUI
-	                            .GameContainer.DesktopContainer.Typebar.Typebox.Text;
+                                .GameContainer.DesktopContainer.Typebar.Typebox.Text;
     local Letters = "";
     print('---------')
     for i, v in pairs(plr.PlayerGui.GameUI.Container.GameSpace.DefaultUI
@@ -434,6 +575,6 @@ function WordBombActivate()
 end
 
 function WordBombClear()
-	if not Modules["GamesWBType"] then return end
-	alreadyUsed = {}
+    if not Modules["GamesWBType"] then return end
+    alreadyUsed = {}
 end
